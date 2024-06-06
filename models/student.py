acf3080b7d08b5c8800110fd354c0f3dd5ac5ecb
Guilherme Gomes <guilherme.gsr.02@gmail.com>
2024-06-05 21:33:59 -0300

    Modification of the student object

--- models/student.py
+++ models/student.py
@@ -1,36 +1,36 @@
 class Student:
     # Constructor to initialize the Student object with id, preferential projects, and grade
     def __init__(self, id, preferential_projects, grade):
-        self._id = id
-        self._preferential_projects = preferential_projects
-        self._grade = grade
+        self.__id = id
+        self.__preferential_projects = preferential_projects
+        self.__grade = grade
 
     # Getter method for id
     @property
     def id(self):
-        return self._id
+        return self.__id
     
     # Setter method for id
     @id.setter
     def id(self, id):
-        self._id = id
+        self.__id = id
 
     # Getter method for preferential projects
     @property
     def preferential_projects(self):
-        return self._preferential_projects
+        return self.__preferential_projects
     
     # Setter method for preferential projects
     @preferential_projects.setter
     def preferential_projects(self, preferential_projects):
-        self._preferential_projects = preferential_projects
+        self.__preferential_projects = preferential_projects
 
     # Getter method for grade
     @property
     def grade(self):
-        return self._grade
+        return self.__grade
     
     # Setter method for grade
     @grade.setter
     def grade(self, grade):
-        self._grade = grade
+        self.__grade = grade
